c6971f5bd3791dfdb4d4b57b3a5390db131c0c4b
fahmi <fahmi@mail.odisys.id>
2018-01-26 15:27:38 +0700

    delete yg gak penting

--- application/views/content_header.php
+++
@@ -1,9 +0,0 @@
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        <?php echo $title ;?>
-        <small></small>
-      </h1>
-
-      <?php $this->load->view('breadcrumbs') ;?>
-    </section>
--- application/views/footer_credit.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-	<!--
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    -->
-    <strong>KarirKu</strong> &minus; Copyright &copy; 2017.
-</footer>
